aa7144e6edb19072961ddc499ef3edb8e47a6ca1
Zach0702 <50173310+Zach0702@users.noreply.github.com>
2019-10-28 21:38:11 -0400

    start stuff

---
+++ StoreMVC/DAL/IInventoryStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StoreMVC.DAL
+{
+    public interface IInventoryStore
+    {
+
+    }
+
+    public class InventoryStore
+    {
+        private readonly object _config;
+
+        public InventoryStore(StoreMVCConfiguration config)
+        {
+            _config = config.Database;
+        }
+    }
+}
