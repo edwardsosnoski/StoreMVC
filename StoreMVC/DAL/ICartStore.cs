c7b670eb3f0f866d638856c16ecbb940825189b3
edwardsosnoski <edward.sosnoski@gmail.com>
2019-10-30 21:20:11 -0400

    Added cart store

---
+++ StoreMVC/DAL/ICartStore.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using Dapper;
+using StoreMVC.Models;
+
+namespace StoreMVC.DAL
+{
+    public interface ICartStore
+    {
+        IEnumerable<CartDALModel> SelectAllInCart();
+    }
+
+    public class CartStore : ICartStore
+    {
+        private readonly Database _config;
+
+        public CartStore(StoreMVCConfiguration config)
+        {
+            _config = config.Database;
+        }
+
+        public IEnumerable<CartDALModel> SelectAllInCart()
+        {
+            var sql = @"SELECT * FROM cart";
+            using (var connection = new SqlConnection(_config.ConnectionString))
+            {
+                var result = connection.Query<CartDALModel>(sql) ?? new List<CartDALModel>();
+                return result;
+            }
+        }
+    }
+}
---
+++ StoreMVC/Models/CartDALModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace StoreMVC.Models
+{
+    public class CartDALModel
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
